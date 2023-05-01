68334456b4c49f8fe4863a76ba9236598f323818
thvmulle <Thomas.VanMullem@UGent.be>
2023-05-01 22:54:46 +0200

    Barchart with monthly mean, tooltip and adapting x-axis

--- src/components/TotalBarChart.vue
+++ src/components/TotalBarChart.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="totalBarCharWrapper">
         <!-- Dropdown used for all the categories -->
         <select id="selectButtonTotalBarChart"></select>
         <!-- Barchart itself -->
@@ -11,8 +11,8 @@
 import * as d3 from "d3";
 
 const margin = {top: 20, right: 30, bottom: 40, left: 90}
-const WIDTH = window.innerWidth / 2;
-const HEIGHT = window.innerHeight / 2;
+const WIDTH = 960 - margin.left - margin.right;
+const HEIGHT = 320 - margin.top - margin.bottom;
 
 export default {
     name: "TotalBarChart",
@@ -21,27 +21,71 @@ export default {
         crimeTypes: Set,
     },
     mounted() {
+        // --------------------------  create a tooltip --------------------
+        const tooltip = d3.select("#totalBarChart")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
+            .style("position", "absolute");
+
+        const mouseover = function(event, d) {
+            const subgroupName = d.year;
+            const subgroupValue = d.total;
+            tooltip
+                .html("Year: " + subgroupName + "<br>" + "Monthly average: " + subgroupValue)
+                .style("opacity", 1)
+        }
+        const mousemove = function(event, _) {
+            tooltip.style("transform","translateY(-55%)")
+                .style("left",((event.pageX)+20)+"px")
+                .style("top",(event.pageY)+"px")
+        }
+        const mouseleave = function(_, __) {
+            tooltip
+                .style("opacity", 0)
+        }
+
         // --------------------------  bar chart --------------------
 
         const svg = d3.select("#totalBarChart")
             .append("svg")
-            .attr("width", "40em")
-            .attr("height", "40em")
+            .attr("width", WIDTH + margin.left + margin.right)
+            .attr("height", HEIGHT + margin.top + margin.bottom)
             .append("g")
             .attr("transform", `translate(${margin.left}, ${margin.right})`);
 
         const allFeatures = this.allFeatures;
+
+        const years = [...new Set(allFeatures.map(item => item["year"]))];
+        const months = getNumberOfMonths();
+
+        function getNumberOfMonths() {
+            const nrOfMonths = {}
+            for (const year of years) {
+                nrOfMonths[year] = [...new Set(allFeatures.filter(element => {
+                    return element["year"] === year;
+                }).map(item => item["month"]))].length
+            }
+
+            return nrOfMonths;
+        }
 
         function getCategoryData(category) {
             const categoryData = allFeatures.filter(element => {
                 return element["fact_category"] === category;
             });
 
-            const years = [...new Set(categoryData.map(item => item["year"]))];
-
             let counts = {};
             for (const year of years) {
                 counts[year] = 0;
+                months[year] = [...new Set(categoryData.filter(element => {
+                    return element["year"] === year;
+                }).map(item => item["month"]))].length
             }
 
             categoryData.forEach(obj => {
@@ -52,7 +96,7 @@ export default {
             let categoryList = [];
 
             for (const [year, total] of Object.entries(counts)) {
-                categoryList.push({year: year, total: total});
+                categoryList.push({year: year, total: Math.ceil(total/months[year])});
             }
 
             return categoryList;
@@ -60,31 +104,39 @@ export default {
 
         function changeCategory(category) {
             const data = getCategoryData(category);
+
+            let max = Math.max(...data.map(obj => obj.total));
+            if (max === 0) {
+                max = 10;
+            }
+            // rescale x-axis
+            x.domain([0, max]);
+            axis.transition()
+                .duration(2000)
+                .call(d3.axisBottom(x));
+
             const bars = svg.selectAll("rect").data(data);
 
             bars.join("rect")
                 .transition()
                 .duration(1000)
-                .attr("x", d => x(0))
+                .attr("x", x(0))
                 .attr("y", d => y(d.year))
                 .attr("width", d => x(d.total))
                 .attr("height", y.bandwidth())
-                .attr("fill", "#69b3a2");
+                .attr("fill", "#31688e");
         }
 
         const data = getCategoryData("Autodiefstal");
 
         // Add X axis
         const x = d3.scaleLinear()
-            .domain([0, 200])
+            .domain([0, 20])
             .range([ 0, WIDTH]);
 
         const axis = svg.append("g")
             .attr("transform", `translate(0, ${HEIGHT})`)
-            .call(d3.axisBottom(x))
-            .selectAll("text")
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
+            .call(d3.axisBottom(x));
 
         // Y axis
         const y = d3.scaleBand()
@@ -103,7 +155,10 @@ export default {
             .attr("y", d => y(d.year))
             .attr("width", d => x(d.total))
             .attr("height", y.bandwidth())
-            .attr("fill", "#69b3a2");
+            .attr("fill", "#31688e")
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave);
 
         //--------------------- dropdown ----------------------------------------
 
@@ -131,5 +186,10 @@ export default {
 </script>
 
 <style scoped>
-
+#totalBarCharWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 2em;
+}
 </style>

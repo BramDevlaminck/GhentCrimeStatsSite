1b4d88664670ef7234f8d57978cdb758d9452caa
Bram Devlaminck <bram.devlaminck@gmail.com>
2023-05-05 14:33:58 +0200

    simple bash script to pull data from portal

---
+++ pull_data_from_portal.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+years=(2018 2019 2020 2021 2022 2023)
+
+for year in ${years[@]}; do
+wget -O "Datasets/criminaliteitscijfers-per-wijk-per-maand-gent-${year}.geojson" "https://data.stad.gent/api/explore/v2.1/catalog/datasets/criminaliteitscijfers-per-wijk-per-maand-gent-${year}/exports/geojson"
+done

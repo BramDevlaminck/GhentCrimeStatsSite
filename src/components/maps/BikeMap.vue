d198f006cec51116cbd15b739288511bc65c7f9e
Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
2023-05-08 22:00:20 +0200

    Update src/components/maps/BikeMap.vue
    Co-authored-by: Thomas Van Mullem <99752211+thvmulle@users.noreply.github.com>

--- src/components/maps/BikeMap.vue
+++ src/components/maps/BikeMap.vue
@@ -215,7 +215,7 @@ export default {
             </p>
             <p>
                 Dit komt omdat de bestaande fietsenstallingen vaak <b>vol</b> zitten, of omdat de dichtste fietsenstalling <b>niet vlakbij</b> het restaurantje,… is waar men wil zitten.
-                In de stationsbuurt is dit een minder probleem aangezien de meeste mensen die hun fiets hier willen plaatsen allemaal op dezelfde plaats moeten zijn: station Gent Sint-Pieters, de <b>grote fietsenstalling bij het station is dus ideaal gepositioneerd</b>.
+                In de stationsbuurt komt dit minder voor aangezien de meeste mensen die hun fiets hier willen plaatsen allemaal op dezelfde plaats moeten zijn: station Gent Sint-Pieters, de <b>grote fietsenstalling bij het station is dus ideaal gepositioneerd</b>.
             </p>
         </div>
 

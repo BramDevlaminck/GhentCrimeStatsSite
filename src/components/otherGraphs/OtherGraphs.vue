88478dd4b4e440188abe0364b58331f9e7b5e989
Thomas Van Mullem <99752211+thvmulle@users.noreply.github.com>
2023-05-08 14:47:43 +0200

    Added titles and info about the graphs (#30)
    * Added titles and info about the graphs
    
    * Update slider text
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
    
    * Update text fietsgrafiek
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>

--- src/components/otherGraphs/OtherGraphs.vue
+++ src/components/otherGraphs/OtherGraphs.vue
@@ -14,7 +14,21 @@ export default {
 
 <template>
     <div>
+        <h1>
+            Maandelijks overzicht van misdrijven per categorie per jaar
+        </h1>
+        <h5>
+            Deze grafiek geeft een overzicht van het totaal aantal voorvallen van een misdrijf per maand en per jaar. <br>
+            De dropdown kan gebruikt worden om een categorie te selecteren en door op de legende te klikken kunnen bepaalde jaren verborgen worden.
+        </h5>
         <TotalLineChart :data="combinedData" :crime-types="crimeTypes"/>
+        <h1>
+            Maandelijks gemiddelde van misdrijven per categorie per jaar
+        </h1>
+        <h5>
+            Deze grafiek geeft een overzicht van het gemiddeld aantal voorvallen van een misdrijf per maand en dat voor elk jaar. <br>
+            De dropdown kan gebruikt worden om een categorie te selecteren.
+        </h5>
         <TotalBarChart :all-features="combinedData" :crime-types="crimeTypes"/>
     </div>
 </template>

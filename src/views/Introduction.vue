27c0f09441eadc253f4681e07eb8f5dc5e808334
Bram Devlaminck <bram.devlaminck@gmail.com>
2023-05-12 00:35:51 +0200

    initial version of introduction text

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,5 +1,48 @@
 <template>
     <div class="container-lg">
-        <h1>Introduction Page</h1>
+        <h1>Introductie</h1>
+        <p>
+            Als project voor het vak <b>Datavisualisatie</b> aan UGent (gegeven door prof. dr. Bart Mesuere) is het de bedoeling om een dataset te toegankelijker/interpreteerbaarder te maken aan de hand van visualisaties.
+            Een eerste stap was het zoeken van een geschikte dataset die ons interesseerde.
+        </p>
+        <p>
+            Onze bedoeling was om te werken met data die geen ver-van-mijn-bedshow is voor de meeste mensen.
+            Aangezien stad <b>Gent</b> heel veel data beschikbaar maakt voor iedereen leek ons dit een goeie plaats om een interessant onderwerp te vinden.
+            Deze datasets gaan over een grote variëteit aan onderwerpen.
+            Zo zijn er datasets die eerder een culturele waarde (UiT in Gent) hebben, andere dan weer praktisch (Ivago).
+            Uiteindelijk viel 1 categorie van datasets viel ons op.
+            Dit waren datasets ter beschikking gesteld door de <b>politie</b> en deze bevatten een overzicht per maand van geregistreerde feiten per wijk.
+        </p>
+        <p>
+            De site van het dataportaal maakt de bestaande datasets beschikbaar in vele formaten:
+        </p>
+        <ul>
+            <li>Interactief aan de hand van tabellen</li>
+            <li>Via een API</li>
+            <li>Als aparte datasets in verschillende formaten (CSV, Excel, JSON, GEOJSON, Shapefile, KML)</li>
+            <li>Als kaart</li>
+            <li>Via een interactieve en aanpasbare visualisatie</li>
+        </ul>
+        <p>
+            Deze laatste 2 zijn kunnen erg interessant zijn om de data <b>toegankelijk</b> te maken naar het brede publiek.
+            Ons viel echter al snel op dat deze opties best ingewikkeld zijn om mee te werken en niet in 1 oog opzicht de data kunnen voorstellen.
+            Bovendien is het rechtuit niet mogelijk om meerdere datasets samen te visualiseren.
+            Iets wat ons heel erg interesseerde omdat het ons toe zou laten te zien hoe het aantal geregistreerde feiten <b>evolueert doorheen de tijd</b>.
+        </p>
+        <p>
+            Enkele vragen die direct bij ons opkwamen toen we de dataset aan het verkennen waren (en die we hopen te beantwoorden zijn):
+        </p>
+        <ul>
+            <li>Hoeveel fietsdiefstallen zijn er effectief en in welke regio's is dit vooral?</li>
+            <li>Welke wijken hebben een verhoogd aantal geregistreerde feiten vergeleken met de rest van Gent?</li>
+            <li>Wat is de invloed van corona en de Gentse feesten op de data?</li>
+            <li>Als kaart</li>
+            <li>Via een interactieve en aanpasbare visualisatie</li>
+        </ul>
+
+        <p>
+            Onze effectieve analyses zijn terug te vinden op het statistieken-tab van deze site.
+            Wat meer informatie over ons en alle gebruikte datasets is terug te vinden op het info-tab.
+        </p>
     </div>
 </template>

169bec65ed564a66080b3f5e4290b77569a2c75f
Bram Devlaminck <bram.devlaminck@gmail.com>
2023-05-11 10:47:04 +0200

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,20 @@ import {defineConfig, loadEnv} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import topLevelAwait from "vite-plugin-top-level-await";
 
-export default (defineConfig(({command, mode}) => {
-    process.env = Object.assign(process.env, loadEnv(mode, command));
-
-    return {
-        plugins: [vue(), topLevelAwait({
-            // The export name of top-level await promise for each chunk module
-            promiseExportName: "__tla",
-            // The function to generate import names of top-level await promise in each chunk module
-            promiseImportName: i => `__tla_${i}`
-        })],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            }
-        },
-        base: process.env.VITE_BASE_URL,
-    };
-
-}));
+export default ({mode}) => {
+    Object.assign(process.env, loadEnv(mode, process.cwd()))
+    return defineConfig( {
+            plugins: [vue(), topLevelAwait({
+                // The export name of top-level await promise for each chunk module
+                promiseExportName: "__tla",
+                // The function to generate import names of top-level await promise in each chunk module
+                promiseImportName: i => `__tla_${i}`
+            })],
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url))
+                }
+            },
+            base: process.env.VITE_BASE_URL,
+        });
+}

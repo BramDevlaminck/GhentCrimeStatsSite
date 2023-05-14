5149d6e60d2026f4892b662ec4ca3bc36772df2d
Bram Devlaminck <bram.devlaminck@gmail.com>
2023-05-14 08:52:25 +0200

    cleanup import

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Introduction from "@/views/Introduction.vue";
 import About from "@/views/About.vue";
 import Statistics from "@/views/Statistics.vue";

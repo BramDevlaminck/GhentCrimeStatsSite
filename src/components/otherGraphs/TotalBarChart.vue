820c4e65196ebcc69ffb9f4115bb20ae4d9515b7
Thomas Van Mullem <99752211+thvmulle@users.noreply.github.com>
2023-05-14 20:14:08 +0200

    Add text for total bar chart (#55)
    * Add text for total bar chart
    
    * Update src/components/otherGraphs/TotalBarChart.vue
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
    
    * Update src/components/otherGraphs/TotalBarChart.vue
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
    
    * Update src/components/otherGraphs/TotalBarChart.vue
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
    
    * Update src/components/otherGraphs/TotalBarChart.vue
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bram Devlaminck <68779933+BramDevlaminck@users.noreply.github.com>

--- src/components/otherGraphs/TotalBarChart.vue
+++ src/components/otherGraphs/TotalBarChart.vue
@@ -1,9 +1,45 @@
-<template>
-    <div id="chartWrapper">
-        <!-- Dropdown used for all the categories -->
-        <select id="selectButtonTotalBarChart"></select>
-        <!-- Barchart itself -->
-        <div id="totalBarChart"/>
+<template xmlns="http://www.w3.org/1999/html">
+    <div id="textChartWrapper">
+        <div id="chartWrapper">
+            <!-- Dropdown used for all the categories -->
+            <select id="selectButtonTotalBarChart"></select>
+            <!-- Barchart itself -->
+            <div id="totalBarChart"/>
+        </div>
+        <div id="text-next-to-slider">
+            <h5>Corona</h5>
+            <p>
+                Als we naar de grafiek kijken over alle categorieën zien we een daling in de jaren van de coronacrisis.
+                Zoals eerder vermeld is dit vrij logisch, mensen waren verplicht binnen te blijven in het begin van 2020 en werden daarna beperkt in hun vrijheid.
+                We zien deze daling bij alle soorten <b>diefstal</b> (gewapend, fiets, ongewapend), <b>inbraken</b> in handelszaken en ook in het aantal <b>parkeerovertredingen</b>. <br>
+                Het meest opvallende hier is het aantal <b> woninginbraken en zakkenrollerij </b>. Mensen werkten van thuis uit en verlieten hun woning minder. Hierdoor waren er minder opportuniteiten om in te breken.
+                Ook de Gentse Feesten zijn deze jaren niet doorgegaan waardoor zakkenrollers minder succesvol waren.
+                Deze cijfers zijn na corona wel grotendeels <b>terug naar hun oude niveau</b> gekomen.
+            </p>
+            <h5>Stijgende lijnen</h5>
+            <p>
+                We hebben een paar misdaden die in een stijgende lijn zitten sinds 2018.
+            </p>
+            <ul>
+                <li>
+                    <b>Bromfietsdiefstal</b>  is verdubbeld vanaf 2021 ten opzichte van 2018
+                </li>
+                <li>
+                    Het aantal gevallen van <b>graffiti</b> is van 8 naar 13 per maand gegaan de laatste jaren
+                </li>
+                <li>
+                    <b>Sluikstorten</b> is van ongeveer 60 maandelijkse gevallen naar meer dan 90 gevallen in 2021 en 2023 gegaan (86 in 2022)
+                </li>
+            </ul>
+            <h5>Dalende lijnen</h5>
+            <p>
+                Gelukkig zien we ook een paar <b>positieve evoluties</b>,
+                autodiefstal is lichtjes afgenomen (9 naar 7 feiten per maand).
+                Geluidshinder daalt de afgelopen jaren standvastig met bijna 10 gevallen per maand
+                en de inbraken in handelszaken zijn na corona laag gebleven in vergelijking met 2018.
+                Ook motordiefstal is de laatste jaren erg beperkt met maar (afgerond) 1 voorval per maand.
+            </p>
+        </div>
     </div>
 </template>
 
@@ -11,7 +47,7 @@
 import * as d3 from "d3";
 
 const margin = {top: 10, right: 30, bottom: 40, left: 60};
-const WIDTH = Math.min(window.innerWidth, 1000) - margin.left - margin.right;
+const WIDTH = Math.min(window.innerWidth, 800) - margin.left - margin.right;
 const HEIGHT = Math.min(window.innerHeight / 2, 320) - margin.top - margin.bottom;
 
 export default {
@@ -40,7 +76,7 @@ export default {
             const subgroupName = d.year;
             const subgroupValue = d.total;
             tooltip
-                .html("Year: " + subgroupName + "<br>" + "Monthly average: " + subgroupValue)
+                .html("Jaar: " + subgroupName + "<br>" + "Maandelijks gemiddelde: " + subgroupValue)
                 .style("opacity", 1);
         };
         const mousemove = function (event, _) {

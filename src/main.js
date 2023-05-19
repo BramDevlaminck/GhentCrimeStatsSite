308d7503651483643157905236f0f2dc879e6920
thvmulle <Thomas.VanMullem@UGent.be>
2023-05-19 11:34:37 +0200

    Move slider css to slider.css

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router';// loads from src/router/index.js
 
 import './assets/main.css'
 import './assets/patterns.css'
+import './assets/slider.css'
 
 createApp(App).use(router).mount('#app');
